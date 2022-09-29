38c038cc84d8bc4dd33af46a44592846ef87095a
juanvalag <juanvalag@unisabana.edu.co>
2022-09-29 18:13:39 -0500

    Adding game properties to component

--- src/components/Games/Game/Game.jsx
+++ src/components/Games/Game/Game.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 import GameStat from './Stats/GameStat'
 
-function Game(props) {
+function Game({ game }) {
     return (
         <div className="card p-5">
             <div className="row mb-3">
-                <div className="col">
-                    <img src="" alt="" />
+                <div className="col d-flex align-items-center justify-content-center">
+                    <img src={game.image} alt="Game Screen" style={{ width: '100%' }} />
                 </div>
                 <div className="col px-1">
                     <div className="row">
                         <div className="col">
-                            <h2>Game</h2>
+                            <h2>{game.title}</h2>
                             <h4>Description</h4>
-                            <p>This is a description of the video game</p>
+                            <p>{game.description}</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
                             <div className="col-12">
                                 <h4>Platform</h4>
-                                <p>PlayStation</p>
+                                <p>{game.platform}</p>
                             </div>
                             <div className="col-12">
                                 <h4>Release date</h4>
-                                <p>2019-10-04</p>
+                                <p>{game.releaseDate}</p>
                             </div>
                         </div>
                         <div className="col">
                             <div className="col-12">
                                 <h4>Genre</h4>
-                                <p>Shooter</p>
+                                <p>{game.genre}</p>
                             </div>
                             <div className="col-12">
                                 <h4>Updated at</h4>
-                                <p>2022-09-15</p>
+                                <p>{game.updatedAt}</p>
                             </div>
                         </div>
                     </div>
@@ -42,16 +42,16 @@ function Game(props) {
             </div>
             <div className="row">
                 <div className="col">
-                    <GameStat value={80} statName={"Players"} />
+                    <GameStat value={game.playingCount} statName={"Players"} />
                 </div>
                 <div className="col">
-                    <GameStat value={80} statName={"Players"} />
+                    <GameStat value={game.backlogCount} statName={"Players"} />
                 </div>
                 <div className="col">
-                    <GameStat value={80} statName={"Players"} />
+                    <GameStat value={game.beatCount} statName={"Players"} />
                 </div>
                 <div className="col">
-                    <GameStat value={80} statName={"Players"} />
+                    <GameStat value={game.retiredCount} statName={"Defeated players"} />
                 </div>
             </div>
         </div>
