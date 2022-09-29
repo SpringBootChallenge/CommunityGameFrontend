ecf1cf416922fb0b48ae2a2c9e1ee7b3dd79ad2d
juanvalag <juanvalag@unisabana.edu.co>
2022-09-29 17:41:30 -0500

    Adding basic game component

---
+++ src/components/Games/Game/Game.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function Game(props) {
+    return (
+        <div className="card p-5">
+            <div className="row">
+                <div className="col">
+                    <img src="" alt="" />
+                </div>
+                <div className="col px-1">
+                    <div className="row">
+                        <div className="col">
+                            <h2>Game</h2>
+                            <h4>Description</h4>
+                            <p>This is a description of the video game</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="col-12">
+                                <h4>Platform</h4>
+                                <p>PlayStation</p>
+                            </div>
+                            <div className="col-12">
+                                <h4>Release date</h4>
+                                <p>2019-10-04</p>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="col-12">
+                                <h4>Genre</h4>
+                                <p>Shooter</p>
+                            </div>
+                            <div className="col-12">
+                                <h4>Updated at</h4>
+                                <p>2022-09-15</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Game
