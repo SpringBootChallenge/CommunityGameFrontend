d6922a7cab7e394e527c606e45785c205f901e54
juanvalag <juanvalag@unisabana.edu.co>
2022-10-01 23:38:53 -0500

    Improving visual hierarchy and image styles to game view

--- src/components/Games/Game/Stats/GameStat.jsx
+++ src/components/Games/Game/Stats/GameStat.jsx
@@ -4,7 +4,7 @@ import './GameStat.css';
 function GameStat({ value, statName }) {
     return (
         <div className='stat-card card w-100 ps-3 pt-2 border-start border-5 d-flex flex-column justify-content-center'>
-            <h1>{value}</h1>
+            <h1 className='text-white'>{value}</h1>
             <p>{statName}</p>
         </div>
     )
